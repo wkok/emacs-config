53d308517800e7fe32a39c760941016559836c8b
Werner Kok <wernerkok@gmail.com>
2023-02-09 21:20:46 +0000

    add eglot-java

--- wk-ide.el
+++ wk-ide.el
@@ -81,4 +81,15 @@
 
 (use-package json-mode)
 
+(use-package eglot-java
+  :pin melpa)
+(add-hook 'java-mode-hook 'eglot-java-mode)
+(add-hook 'eglot-java-mode-hook (lambda ()
+  (define-key eglot-java-mode-map (kbd "C-c l n") #'eglot-java-file-new)
+  (define-key eglot-java-mode-map (kbd "C-c l x") #'eglot-java-run-main)
+  (define-key eglot-java-mode-map (kbd "C-c l t") #'eglot-java-run-test)
+  (define-key eglot-java-mode-map (kbd "C-c l N") #'eglot-java-project-new)
+  (define-key eglot-java-mode-map (kbd "C-c l T") #'eglot-java-project-build-task)
+  (define-key eglot-java-mode-map (kbd "C-c l R") #'eglot-java-project-build-refresh)))
+
 ;;; wk-ide.el ends here
