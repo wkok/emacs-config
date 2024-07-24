00bef052dc0c23c02558a704f8cde8cb1071795f
Werner Kok <werner@hti-systems.co.za>
2024-07-24 09:33:03 +0100

    Ediff shortcut for keep both

--- wk-ide.el
+++ wk-ide.el
@@ -9,6 +9,18 @@
 (use-package magit
   :ensure t)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Add `d' as a means to keep both in ediff
+(defun ediff-copy-both-to-C ()
+  (interactive)
+  (ediff-copy-diff ediff-current-difference nil 'C nil
+                   (concat
+                    (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+                    (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+(defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
+(add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (use-package cider
   :ensure t
   :init
