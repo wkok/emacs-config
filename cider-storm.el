61159016244e8862c7a45b96dd4e0183c8be7468
Werner Kok <werner@hti-systems.co.za>
2024-09-10 18:36:15 +0100

    Add cider-storm

--- cider-storm.el
+++ cider-storm.el
@@ -12,6 +12,7 @@
 
 (add-to-list 'safe-local-variable-values
              '(cider-jack-in-nrepl-middlewares . ("flow-storm.nrepl.middleware/wrap-flow-storm"
+                                                  ("refactor-nrepl.middleware/wrap-refactor" :predicate cljr--inject-middleware-p)
                                                   "cider.nrepl/cider-middleware")))
 
 
